34dd47386342d94abd1571b3e80f228ca1ba8772
Matheus Moreira <69878348+moreira-m@users.noreply.github.com>
2025-12-12 09:03:24 -0300

    feat: analytics configs

--- workspaces/web/src/app/layout.tsx
+++ workspaces/web/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { VisualEditing } from "next-sanity/visual-editing";
 import { draftMode } from "next/headers";
+import Script from "next/script";
 import "./globals.css";
 import "@/styles/global.scss";
 import "@/styles/home.scss";
@@ -22,6 +23,19 @@ export default async function RootLayout({
       <body>
         {children}
         {isEnabled ? <VisualEditing /> : null}
+
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}');
+          `}
+        </Script>
       </body>
     </html>
   );
